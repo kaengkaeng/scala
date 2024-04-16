a2ef9f8d14ff95a3b2fbf49829e802502f1a6d63
kaengkaeng <kangh83@zedat.fu-berlin.de>
2024-04-16 17:26:21 +0200

    Update twofer.scala

--- twofer.scala
+++ twofer.scala
@@ -1,4 +1,8 @@
-def twofer(name: String= "you"): String =
+def twofer(name: String = "you"): String =
+    //default parameter value => name: String = "you"
     name match {
       case _ => "One for "+ name+ ", one for me."
     }
+
+ /*In Scala, default parameter values refer to assigning values to parameters when defining functions or methods. 
+ This means that if no value is provided for a parameter when calling the function, the default value will be used.*/   
