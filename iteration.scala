034560913b6d5c8a13dc0c78589d8595c843649c
kaengkaeng <kangh83@zedat.fu-berlin.de>
2024-04-22 22:35:17 +0200

    Update iteration.scala
    List, String und Array

--- iteration.scala
+++ iteration.scala
@@ -1,3 +1,20 @@
+
+
+//22042024 
+ def filterList(list: List[Any]): List[Int] =
+
+  var num :List[Int] = List()
+  var cha :List[String] = List()
+  
+  //isInstanceOf[Int] => Int class의 하위 클래스 
+  //isInstanceOf[String]=> String class의 하위 클래스 
+  for (x <-  list)
+     if (x.isInstanceOf[Int]) num = num :+ x.asInstanceOf[Int]
+     else if (x.isInstanceOf[String]) cha = cha :+ x.asInstanceOf[String]
+
+  num 
+
+
 //Ausdruck: ganze Zahl in List und danach List ist umgedreht geliefert wie python
 def reverseSeq(n: Int): Seq[Int] = {
   (n to 1 by -1).toList
@@ -23,3 +40,5 @@ def areYouPlayingBanjo2(name: String): String = {
         case _ => name + " does not play banjo"
     
  }
+
+
