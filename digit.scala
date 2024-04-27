9bfe72cdc3998c8e5c4f9ca24ddff8c48d36c75b
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-04-27 21:54:52 +0200

    Array, Seq und List

--- digit.scala
+++ digit.scala
@@ -1,3 +1,20 @@
+/*27042024*/
+// 정수 배열을 입력받아 홀수 번 등장하는 숫자를 찾아 반환하는 함수
+def findOdd(xs: Seq[Int]): Int = 
+    
+    var oddNum = 0 // 홀수 번 등장하는 숫자를 저장할 변수
+    for (x <- xs) { // 배열의 각 요소를 반복합니다.
+      
+      val oddCount = xs.count(_ == x) // 현재 숫자가 배열에서 등장하는 횟수를 세어 oddCount에 할당
+      
+      if (oddCount % 2 != 0) // 만약 현재 숫자가 홀수 번 등장한다면
+        oddNum = x // 홀수 번 등장하는 숫자를 oddNum에 할당
+    } 
+    
+    oddNum // 홀수 번 등장하는 숫자 반환
+
+
+
 /*23042024*/
 
 // A function that calculates the amount of water Nathan will drink during the given time.
