fe86814361c92d8c7369b05c7dae7fe7e10ad4d0
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-05-01 21:43:47 +0200

    option
    option 은 none 과 some으로 나뉜다. 이 차이 구분하기

---
+++ option.scala
@@ -0,0 +1,18 @@
+// 정수 값이 있는 시퀀스에서 첫 번째 비연속 정수를 찾는 함수입니다.
+// 비연속 정수가 발견되면 해당 값을 Some으로 감싸서 반환하고,
+// 발견되지 않으면 None을 반환합니다.
+def firstNonConsecutive(values: Seq[Int]): Option[Int] = {
+    // 시퀀스의 각 요소에 대해 반복합니다.
+  for (i <- 1 until values.length) {
+      // 현재 요소와 이전 요소를 비교하여 연속성을 확인합니다.
+    if (values(i) != values(i - 1) + 1) {
+        // 비연속 정수가 발견되면 해당 값을 Some으로 감싸서 반환합니다.
+        // Some은 값이 있음을 나타내는 옵션입니다.
+      return Some(values(i))
+      }
+    }
+    // 비연속 정수가 발견되지 않으면 None을 반환합니다.
+    // None은 값이 없음을 나타내는 옵션입니다.
+  None
+  }
+
