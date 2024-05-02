a6b0427eff134415144118f1fc92c97043f63a00
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-05-02 17:07:18 +0200

    Rekursion

--- rekursive.scala
+++ rekursive.scala
@@ -1,3 +1,12 @@
+def distinct(xs: Seq[Int]): Seq[Int] = 
+ xs match { 
+   case Nil => List() // 만약 입력이 빈 리스트인 경우, 빈 리스트를 반환합니다.
+   case x :: tail => x +: distinct(tail.filter(_ != x)) 
+   // x를 결과 리스트에 추가, tail에서 x와 같지 않은 요소들로 재귀적으로 함수를 호출, "+:" 리스트 원소 append :) 
+}
+
+
+
 
 /*test
  (0, ""),
