92a0702ea3eb01ff2529ccadf39a0f34b825c4e5
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-05-08 15:52:17 +0200

    Rekusion

--- rekursive.scala
+++ rekursive.scala
@@ -1,3 +1,19 @@
+/*대소문자 섞인 회문 프로그램*/ 
+def isPalindrome(s: String): Boolean = {
+  // 문자열을 대문자로 변환하여 대소문자를 무시하고 처리
+  val neustr = s.toUpperCase
+  (neustr) match {    
+    case _ if neustr.length <= 1 => true // 문자열의 길이가 1 이하인 경우, 회문으로 간주하여 true 반환
+    // 문자열의 첫 번째 문자와 마지막 문자가 같은 경우,
+    // 문자열의 첫 번째와 마지막 문자를 제외한 부분에 대해 재귀적으로 isPalindrome 함수 호출
+    case _ if neustr.head == neustr.last => isPalindrome(neustr.tail.init)
+    // str.tail 은 앞머리를 제외한 나머지 부분이고, str.init 맨뒷부분을 제외한 나머지 부분이다. 
+    case _ => false
+  }
+}
+
+
+
 def summation(n: Int): Int = 
     n match 
       case 0 => 0 
