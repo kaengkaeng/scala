b2bdf95489028c7583c59e2f70cbc0047b3e10e9
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-05-10 14:45:13 +0200

    tailrekursive

--- tailrekursive.scala
+++ tailrekursive.scala
@@ -1,3 +1,16 @@
+// 내림차순 정렬 함수 만들기 
+def descendingOrder(num: Int): Int = {
+    def step(num: Int, acc: Int): Int =
+      num match {
+        case 0 => acc
+        case _ => step(num / 10, acc * 10 + num % 10)
+    }
+    val result = step(num, 0)
+    result.toString.sortWith(_ > _).toInt
+  }
+
+
+
 def number(busStops: List[(Int, Int)]): Int =
   def help(busStops: List[(Int, Int)], acc1:Int, acc2:Int) :Int =
     busStops match
