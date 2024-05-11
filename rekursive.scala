7c3ec5fa64474f0cc5124597a4f0cbb50eed7115
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-05-11 14:18:39 +0200

    Rekursion

--- rekursive.scala
+++ rekursive.scala
@@ -1,3 +1,17 @@
+// 입력된 Long 정수의 자릿수(digit) 를 반환하는 함수
+def digits(n: Long): Int = {
+  def step(n: Long, acc: Int): Int = 
+    n match {      
+      case 0 => acc // 입력값이 0인 경우, 누적된 자릿수(acc) 반환
+      case _ => step(n / 10, acc + 1)// 입력값이 0이 아닌 경우, 자릿수를 하나 증가시키고 재귀적으로 호출
+    }
+
+  // 만약 입력값이 0이면, 최소 한 자릿수가 있으므로 1 반환
+  // 그렇지 않으면, 절댓값을 취한 후 재귀 함수 호출
+  if (n == 0) 1 else step(n.abs, 0)
+}
+
+
 /*대소문자 섞인 회문 프로그램*/ 
 def isPalindrome(s: String): Boolean = {
   // 문자열을 대문자로 변환하여 대소문자를 무시하고 처리
