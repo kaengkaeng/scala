2f76d5d9ebfdf7d544363ba11aafb79df9954b2d
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-05-12 17:58:01 +0200

    higher oder funtkion

--- hof.scala
+++ hof.scala
@@ -1,5 +1,9 @@
 //higher ordner function
 
+//ture counter 
+def countSheep(sheep: Array[Boolean]): Int = 
+    sheep.filter(_ == true).count(_ == true)
+
 //제일 작은 숫자 두개 in List 합해서 출력하시오. 
 def sumTwoSmallest(numbers: List[Int]): Int = 
     numbers.sorted.take(2).sum
