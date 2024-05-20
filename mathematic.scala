b74d1302a75e2658cc818625ad9b492ec864f904
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-05-20 23:07:28 +0200

    Math

--- mathematic.scala
+++ mathematic.scala
@@ -1,3 +1,8 @@
+// 다각형 내부 각도 구하는 함수 
+// the sum of internal angles 
+def angle(n: Int): Int = (n-2)*180 
+
+
 def predictAge(age1: Int, age2: Int, age3: Int, age4: Int, age5: Int, age6: Int, age7: Int, age8: Int): Int = {
     
     val newList: List[Int] = List(age1, age2, age3, age4, age5, age6, age7, age8)
