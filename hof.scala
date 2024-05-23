638f4a0e7ee7855157a2344df8aa55d6bd644ef6
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-05-23 15:09:28 +0200

    hof :)

--- hof.scala
+++ hof.scala
@@ -1,5 +1,12 @@
 //higher ordner function
 
+//구구단 함수 
+def multiTable(n: Int): String = 
+  (1 to 10).map(i => s"$i * $n = ${i* n}").mkString("\n")
+  // 범위 생성 : (1 to 10)
+  // map 활용?!
+  // mkString: 문자열로 결합 후  한줄 띄기 ("\n")
+
 // 문장에서 제일 짧은 단어 찾기 
 def findShort(str: String): Int = str.split(" ").map(_.length).min 
 
