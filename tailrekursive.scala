3a7fade7473c04c808cf87e394646b3496a1283f
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-05-29 17:38:59 +0200

    tailrekursive

--- tailrekursive.scala
+++ tailrekursive.scala
@@ -1,3 +1,22 @@
+
+// 주어진 숫자 n을 확장된 형태로 반환하는 함수 : (input "12" -> output "10+2") 
+
+def expandedForm(n: Long): String =      
+  def step(n: Long, acc: Long): String = { // 내부 헬퍼 함수 step 정의. n과 현재 자리수를 추적하는 acc를 매개변수로 가짐
+    (n, acc) match                          //  (n, acc)에 대한 패턴 매칭 시작
+      case (0, _) => ""                     //  n이 0인 경우, 빈 문자열을 반환
+      case (_, _) =>                        //  그 외의 경우에 대해 처리
+        val next = step(n / 10, acc * 10)   //  재귀 호출: n을 10으로 나눈 값과 acc에 10을 곱한 값을 인수로 사용
+        val current = (n % 10) * acc        //  현재 자리수의 값을 계산
+        if (current == 0) next              // 현재 자리수가 0이면 next 반환
+        else if (next.isEmpty) s"$current"  // next가 빈 문자열이면 현재 자리수의 값을 문자열로 반환
+        else s"$next + $current"            // next가 빈 문자열이 아니면 현재 자리수의 값을 next에 덧붙여 반환
+  }
+
+  if (n == 0) "0"                           // n이 0이면 "0"을 반환
+  else step(n, 1)                           // 그렇지 않으면, step 함수 호출. acc 초기값은 1
+
+
 // 내림차순 정렬 함수 만들기 
 def descendingOrder(num: Int): Int = {
     def step(num: Int, acc: Int): Int =
